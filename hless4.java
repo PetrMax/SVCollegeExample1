00855c429719b82bb179505ade4a40ce6678f14d
CCLondon25 <yanivmy@gmail.com>
2019-05-22 20:08:46 +0300

    fjkghi

---
+++ hless4.java
@@ -0,0 +1,97 @@
+package test;
+import java.util.Scanner;
+public class hless4 {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+
+//***tar1***
+//
+//System.out.println ("Please enter 5 numbers into array");
+//int []arr = new int[5];
+//int size = arr.length;
+//for (int i=0 ; i<size ; i++) {
+//arr[i] = input.nextInt();
+//						     }
+//for(int i=0 ;i<size ; 	i++){ 
+//System.out.println(arr[i]);
+//								}	
+//}
+//}
+
+
+//***tar2***
+//
+//System.out.println ("Please enter 10 letters into array");
+//String []arr = new String[10];
+//int size = arr.length;
+//for (int i=0 ; i<size ; i++) {
+//arr[i] = input.nextLine();
+//						     }
+//for(int i=0 ;i<size ; i++){ 
+//
+//	if (arr[i]>='A' && arr[i] <='Z') {
+//		System.out.println(arr[i]);
+//					}	
+//}
+//
+//}
+//}
+
+
+//***tar3***
+//
+//String []arr = {"a","c","D","e","f","G"} ;
+//int size = arr.length;			
+//String []arr2 = new String[6];
+//int size2 = arr2.length;
+//for (int i=0 ; i<size2 ; i++) {
+//	if (arr[i]>='A' && arr[i] <='Z') {
+//arr2[i] = " " + arr[i];
+//System.out.println(arr2[i]);    
+//										}
+//							}                        
+//}
+//}
+
+//***tar4***
+//
+//System.out.println ("Please enter 10 numbers into array");
+//int []arr = new int[10];
+//int size = arr.length;
+//int max=arr[0];
+//for (int i=0 ; i<size ; i++) {
+//System.out.println("Enter any number");
+//arr[i] = input.nextInt();
+//						     }
+//for(int i=0 ;i<size ; 	i++){ 
+//	if(max<arr[i]) {
+//		max=arr[i];
+//					}
+//	}
+//System.out.println("The biggest number in the array is: "+max);
+//}
+//}
+
+
+//***tar5***
+//
+//System.out.println ("Please enter 7 numbers into array");
+//int []arr = new int[7];
+//int size = arr.length;
+//int counteEven=0;
+//for (int i=0 ; i<size ; i++) {
+//arr[i] = input.nextInt();
+//if(arr[i]%2 == 0) {
+//	counteEven++;
+//					}
+//							}
+//
+//System.out.println("The amount of even numbers is: "+counteEven);
+//
+//}
+//}
+//
+
+
+
